392272e4e91121201fa89eb60a09fcca021ade7d
alfian <devsroot8@gmail.com>
2023-10-05 21:24:00 +0700

    feat(script.js) add dark theme handler

--- script/script.js
+++ script/script.js
@@ -1,13 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu-list");
+const toggleBtn = document.querySelector(".theme-toggle");
 
+// nav for mobile screen
 hamburger.addEventListener("click", mobileMenu);
-
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
 
+// add border when scroll down
 window.onscroll = () => {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
@@ -18,3 +20,21 @@ window.onscroll = () => {
     header.style.borderBottom = "none";
   }
 };
+
+// state
+const theme = localStorage.getItem("theme");
+
+// on mount
+theme && document.body.classList.add(theme);
+
+// dark theme
+const themeHandler = () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark-mode");
+  } else {
+    localStorage.removeItem("theme");
+  }
+};
+
+toggleBtn.addEventListener("click", themeHandler);
